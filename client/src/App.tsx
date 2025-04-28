f007fb05af157d314014c55a393a9bd92924e8a6
Sean Takahashi <seanyuto@gmail.com>
2025-04-28 17:41:19 -0400

    added new pages and features

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,7 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { Toaster } from 'react-hot-toast';
+
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import PostsPage from './pages/PostsPage';
@@ -19,25 +22,30 @@ import ResultsPage from './pages/ResultsPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/posts" element={<PostsPage />} />
-      <Route path="/minutes" element={<MinutesPage />} />
-      <Route path="/student-gov" element={<StudentGovPortal />} />
-      <Route path="/post-announcement" element={<PostAnnouncementPage />} />
-      <Route path="/create-post" element={<CreatePostPage />} />
-      <Route path="/write-minutes" element={<WriteMinutesPage />} />
-      <Route path="/students" element={<StudentsListPage />} />
-      <Route path="/elections" element={<ElectionPortal />} />
-      <Route path="/elections/create" element={<CreateElectionPage />} />
-      <Route path="/elections/overview" element={<ElectionOverviewPage />} />
-      <Route path="/elections/review" element={<CandidacyReviewPage />} />
-      <Route path="/elections/candidates" element={<CandidatesListPage />} />
-      <Route path="/elections/submit" element={<SubmitCandidacyPage />} />
-      <Route path="/elections/vote" element={<VotingPage />} />
-      <Route path="/elections/results" element={<ResultsPage />} />
-    </Routes>
+    <Router>
+      <Toaster position="top-center"/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/minutes" element={<MinutesPage />} />
+        <Route path="/student-gov" element={<StudentGovPortal />} />
+        <Route path="/post-announcement" element={<PostAnnouncementPage />} />
+        <Route path="/create-post" element={<CreatePostPage />} />
+        <Route path="/write-minutes" element={<WriteMinutesPage />} />
+        <Route path="/students" element={<StudentsListPage />} />
+        <Route path="/elections" element={<ElectionPortal />} />
+        <Route path="/elections/create" element={<CreateElectionPage />} />
+        <Route path="/elections/overview" element={<ElectionOverviewPage />} />
+        <Route path="/elections/review" element={<CandidacyReviewPage />} />
+        <Route path="/elections/candidates" element={<CandidatesListPage />} />
+        <Route path="/elections/submit" element={<SubmitCandidacyPage />} />
+        <Route path="/elections/vote" element={<VotingPage />} />
+        <Route path="/elections/results" element={<ResultsPage />} />
+      </Routes>
+    </Router>
+
   );
 }
 
