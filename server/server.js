cd95c79aeb0804330e5abfa109fc49b44f3a930c
Sean Takahashi <seanyuto@gmail.com>
2025-04-28 01:30:37 -0400

    add new files

--- server/server.js
+++ server/server.js
@@ -1,26 +1,16 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const cors = require('cors');
+require('dotenv').config();
 
-dotenv.config();
 const app = express();
-
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("Mongo error: ", err));
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('Mongo connected'));
 
-// Example route
-app.get('/', (req, res) => {
-  res.send('Hello from Hoshuko server!');
-});
+app.use('/api/auth', authRoutes);
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
