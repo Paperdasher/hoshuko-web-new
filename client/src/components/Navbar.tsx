a9e38c3b12d1c75bd976c2d60d60e758b1691075
Sean Takahashi <seanyuto@gmail.com>
2025-04-29 01:23:24 -0400

    translated to japanese, need to style better

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -23,16 +23,16 @@ export default function Navbar() {
   return (
     <nav className="bg-blue-600 text-white px-6 py-4 flex justify-between items-center">
       <div className="flex space-x-4">
-        <Link to="/" className="font-bold text-lg hover:underline">Home</Link>
-        <Link to="/about" className="hover:underline">About</Link>
-        <Link to="/posts" className="hover:underline">Posts</Link>
-        <Link to="/minutes" className="hover:underline">Minutes</Link>
+        <Link to="/" className="font-bold text-lg hover:underline">ホーム　</Link>
+        <Link to="/about" className="hover:underline">生徒会とは　</Link>
+        <Link to="/posts" className="hover:underline">ニュース　</Link>
+        <Link to="/minutes" className="hover:underline">議事録　</Link>
         {user?.position && (
           <Link to="/student-gov-portal" className="hover:underline">
-            Gov Portal
+            生徒会ポータル
           </Link>
         )}
-        <Link to="/elections" className="hover:underline">Elections</Link>
+        <Link to="/elections" className="hover:underline">選挙</Link>
       </div>
 
       <div className="flex space-x-4">
@@ -43,12 +43,12 @@ export default function Navbar() {
               onClick={handleLogout}
               className="bg-white text-blue-600 px-3 py-1 rounded hover:bg-gray-100"
             >
-              Logout
+              ログアウト
             </button>
           </>
         ) : (
           <Link to="/login" className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-100">
-            Login
+            ログイン
           </Link>
         )}
       </div>
