f6d5c5ddd5b4c319a26d0f09e47694562b9ec58e
Sean Takahashi <seanyuto@gmail.com>
2025-04-29 00:33:14 -0400

    updated

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth');
-const cors = require('cors');
 require('dotenv').config();
 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI).then(() => console.log('Mongo connected'));
+// Example route
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
 
-app.use('/api/auth', authRoutes);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
